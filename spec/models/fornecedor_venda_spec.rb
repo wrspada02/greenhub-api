a931e245bfe48cdc94eb64063c7ce18e71a997e0
William Spada <williamrspada02@gmail.com>
2024-11-07 20:13:21 -0300

    test: add fornecedor_venda test

--- spec/models/fornecedor_venda_spec.rb
+++ spec/models/fornecedor_venda_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe FornecedorVenda, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:fornecedor_venda) { build(:fornecedor_venda) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:preco) }
+    it { is_expected.to validate_numericality_of(:preco).is_greater_than(0) }
+  end
 end
