9bc7436827548f2cc1e61e781c380728e7c6fae5
Tilevs <tiago.fernandes9791@gmail.com>
2024-11-22 13:20:30 -0300

    created CRUD for empresas

---
+++ app/controllers/empresas_controller.rb
@@ -0,0 +1,51 @@
+class EmpresasController < ApplicationController
+  before_action :set_empresa, only: %i[show update destroy]
+
+  # GET /empresas
+  def index
+    @empresas = Empresa.all
+    render json: @empresas
+  end
+
+  # GET /empresas/:id
+  def show
+    render json: @empresa
+  end
+
+  # POST /empresas
+  def create
+    @empresa = Empresa.new(empresa_params)
+    if @empresa.save
+      render json: @empresa, status: :created, location: @empresa
+    else
+      render json: @empresa.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /empresas/:id
+  def update
+    if @empresa.update(empresa_params)
+      render json: @empresa
+    else
+      render json: @empresa.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /empresas/:id
+  def destroy
+    @empresa.destroy
+    head :no_content
+  end
+
+  private
+
+  # Set empresa for show, update, and destroy actions
+  def set_empresa
+    @empresa = Empresa.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through
+  def empresa_params
+    params.require(:empresa).permit(:id_empresa, :endereco, :cnpj, :telefone, :nome)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # root "posts#index"
   
   resources :produtos
+  resources :empresas
 end
