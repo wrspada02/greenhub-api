3f64f69cb87046ab3ee8a93dd1e38ef4e777453b
William Spada <williamrspada02@gmail.com>
2024-12-09 21:15:04 -0300

    config: adjust cors config

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,14 @@ module GreenhubApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # Add Rack::Cors middleware for handling CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
